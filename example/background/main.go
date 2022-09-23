4bdf8b71d6b89eefd5f24fc0010e94e494150eb8
vearne <asdwoshiaotian@gmail.com>
2022-09-23 11:12:35 +0800

    reduce dependence (#7)

--- example/background/main.go
+++ example/background/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/vearne/executor"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -49,23 +50,22 @@ type TaskParam struct {
 	curl -XPOST http://localhost:8080/api/task -d '{"param":10}'
 */
 func main() {
-	r := gin.Default()
-	r.POST("/api/task", func(c *gin.Context) {
+	http.HandleFunc("/api/task", func(out http.ResponseWriter, in *http.Request) {
+		bt, _ := io.ReadAll(in.Body)
 		p := TaskParam{}
-		c.BindJSON(&p)
+		json.Unmarshal(bt, &p)
 		task := AsyncTask{param: p.Param}
 		_, err := pool.Submit(&task)
 		if err != nil {
 			log.Println("error", err)
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"code": 0,
-			"msg":  "submit success",
-		})
+		out.WriteHeader(http.StatusOK)
+		out.Write([]byte(`{"code": 0, "msg": "submit success"}`))
 	})
 
-	// 启动web服务，监听9090端口
-	go func() { log.Fatal(r.Run(":8080")) }()
+	log.Println("start web server...")
+	// start web server
+	go func() { log.Fatal(http.ListenAndServe(":8080", nil)) }()
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
